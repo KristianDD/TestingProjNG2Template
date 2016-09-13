8a72ef487586f8c041b1420b888e092775d74b6c
t t <t>
2016-09-13 11:50:08 +0300

    App created.

---
+++ TestingProjNG2Template/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+import { NS_ROUTER_DIRECTIVES } from "nativescript-angular/router";
+
+@Component({
+  selector: "ns-main",
+  template: "<page-router-outlet></page-router-outlet>",
+  directives: [NS_ROUTER_DIRECTIVES]
+})
+export class AppComponent {}
